e9a83b8e70d2e73c17cdf731338f2a56a534bf25
Ilyas Sultanov <10sis@mail.ru>
2018-06-29 07:31:39 +0500

    Update WordModel.java
    interface for work with Retrofit
    , it prescribes the address where we will be knocking and the parameters

--- app/src/main/java/me/geeksploit/markmyword/model/WordModel.java
+++ app/src/main/java/me/geeksploit/markmyword/model/WordModel.java
@@ -1,4 +1,13 @@
 package me.geeksploit.markmyword.model;
+import javax.inject.Inject;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public class WordModel {
     private String word;
@@ -32,5 +41,37 @@ public class WordModel {
 
     public String getImgUri() {
         return imgUri;
+    }
+}
+/**//*интерфейс  для работы с Retrofit
+, в нем прописываем адрес куда будем стучаться и параметры*/
+
+
+public interface API {
+
+    @GET("search/books")
+    Call<PdfItemModel> getSearchedRepos(@Query("q") String q,
+                                       @Query("page") int page,
+                                       @Query("per_page") int perPage);
+}
+/**//*клиент для доступа к запросам.*/
+public class RestClient {
+
+    public static final String BASE_URL = "https://www.libtxt.ru";
+
+    private final API service;
+
+    @Inject
+    public RestClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        service = retrofit.create(API.class);
+    }
+
+    public API getService() {
+        return service;
     }
 }
